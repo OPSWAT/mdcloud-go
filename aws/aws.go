c8602a82baaa831641a8837e49f20cce618c5da8
Vlad Oros <voros@opswat.com>
2017-10-10 17:11:44 +0300

    better error handling on aws credentials

--- aws/aws.go
+++ aws/aws.go
@@ -1,22 +1,23 @@
 package aws
 
 import (
-	"github.com/OPSWAT/mdcloud-go/utils"
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 // Session variable related to the current AWS account
 var (
 	Session *session.Session
-	err     error
 )
 
 // LoadProfile ensures we have a session
 func LoadProfile() {
-	Session, err = session.NewSession()
-	if err != nil {
-		utils.ExitError("error loading session", err)
+	Session = session.Must(session.NewSession())
+	if _, err := Session.Config.Credentials.Get(); err != nil {
+		log.Fatalln("error loading credentials, credentials setup under ~/.aws/credentials")
 	}
+
 	// defaults to us-west-2
 	if *Session.Config.Region == "" {
 		Session.Config = Session.Config.WithRegion("us-west-2")
