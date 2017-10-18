f7df4d2c1495227bcd57972ddb8df24398618a04
Vlad Oros <orosvlad@gmail.com>
2017-10-18 23:40:22 +0300

    fix make build, smaller binaries

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 cd /go/src/github.com/OPSWAT/mdcloud-go \
     && cp -r /tmp/vendor . \
-    && gox -os="linux darwin windows" -ldflags "-X main.VERSION=$VERSION" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
+    && gox -osarch="linux/amd64 linux/386 darwin/amd64 darwin/386 windows/arm windows/386" -ldflags "-X main.VERSION=$VERSION -s -w" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
